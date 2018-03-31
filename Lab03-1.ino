36a3852ff7663fb1eb24d3ed8c401a6891d83bc9
wesley920068 <36829691+wesley920068@users.noreply.github.com>
2018-03-31 14:23:20 +0800

    Add files via upload

---
+++ Lab03-1.ino
@@ -0,0 +1,20 @@
+byte val;
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Hello,World");
+  pinMode(3,OUTPUT);
+ 
+}
+
+void loop() {
+  if(Serial.available()){
+    val = Serial.read();
+    if (val =='1'){
+      digitalWrite(3,HIGH);
+      Serial.println("LED ON");
+    } else if (val =='0') {
+      digitalWrite(3,LOW);
+      Serial.println("LED OFF");
+    }
+  }
+}
