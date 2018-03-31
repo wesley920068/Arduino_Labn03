42951337670bcbf29e028b3aac73143a633c4c5a
wesley920068 <36829691+wesley920068@users.noreply.github.com>
2018-03-31 15:43:40 +0800

    Add files via upload

---
+++ Lab03-2.ino
@@ -0,0 +1,20 @@
+double vin;
+double analogin = 0;
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Hello,World");
+  pinMode(14,INPUT);
+ 
+}
+
+void loop() {
+ for (byte i=0;i<=5;i++)
+  analogin = 0.7* analogin + 0.3* analogRead(A0);
+  vin = analogin * 2.5 / 4095;
+  Serial.print("AnalogRead = ");
+  Serial.print(analogin);
+  Serial.print("vin =");
+  Serial.println(vin);
+  delay(500);
+
+}
---
+++ Lab03-3.ino
@@ -0,0 +1,34 @@
+double vin;
+double analogin = 0;
+double analogin1 = 0;
+double ResisterValue = 0;
+double sensorValue = 0;
+double sensorVoltage = 0;
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Hello,World");
+  pinMode(14,INPUT);
+ 
+}
+
+void loop() {
+ for (byte i=0;i<=5;i++)
+  analogin = 0.7* analogin + 0.3* analogRead(A0);
+  vin = analogin * 2.5 / 4095;
+
+  for (byte i=0;i<=5;i++)
+    sensorValue = 0.7* sensorValue + 0.3* analogRead(A1);
+   sensorVoltage = sensorValue * 5 / 4095;  
+   analogin1 = analogRead(A1);
+    Serial.print(" AnalogRead(A1) = ");
+  Serial.print(analogin1);
+  Serial.print(" vin =");
+  Serial.print(vin);
+  Serial.print(" sensorValue =");
+  Serial.print(sensorValue);
+  Serial.print("  sensorVoltage = ");
+  Serial.println(sensorVoltage);
+  
+  delay(500);
+
+}
